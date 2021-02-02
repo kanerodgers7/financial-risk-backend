3487f245a88f2daae2c54360ccf59073f9f73655
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-02 19:14:47 +0530

    send user specific socket event

--- helper/socket.helper.js
+++ helper/socket.helper.js
@@ -1,22 +1,25 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
 var socket_io = require('socket.io');
 var io = socket_io();
 var socketApi = {};
 socketApi.io = io;
-let socketUser = [];
+let socketUser = {};
 
 io.on('connection', async function (socket) {
     // console.log('New user connected:', socket.id);
-    socketUser.push(socket);
-    // console.log('socket::', socket);
-    // console.log('socket::', socket.handshake);
-    // console.log('socket::', socket.handshake.query);
-    // console.log('TOKEN::', socket.handshake.query.token);
+    socketUser[socket.id] = socket;
     let userToken = socket.handshake.query.token;
     await addSocketIdToUser(userToken, socket.id);
     socket.on("disconnect", async () => {
         // console.log('User disconnected:', socket.id);
         await removeSocketIdFromUser(socket.id);
-        socketUser = socketUser.filter(user => user.id !== socket.id);
+        // socketUser = socketUser.filter(user => user.id !== socket.id);
+        for (let key in socketUser) {
+            if (key.toString() === socket.id) {
+                delete socketUser[key]
+            }
+        }
     });
 });
 
@@ -26,9 +29,13 @@ socketApi.sendNotification = async function ({notificationObj, socketIds = [], u
     if (userId && socketIds.length === 0) {
         socketIds = await getSocketIdFromUser(userId);
     }
-    socketIds.for(socketId => {
-        socketUser[socketId].emit('FromAPI', notificationObj)
-    })
+    if (socketIds && socketIds.length !== 0) {
+        socketIds.for(socketId => {
+            if (socketUser[socketId]) {
+                socketUser[socketId].emit('FromAPI', notificationObj)
+            }
+        })
+    }
 };
 
 let addSocketIdToUser = (token, socketId) => {
