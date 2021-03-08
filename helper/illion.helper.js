ad660fb8dfecfbb8e9751507e3401b84b208b98f
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-08 20:32:00 +0530

    Processed Illion Report

--- helper/illion.helper.js
+++ helper/illion.helper.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const convert = require('xml-js');
+var parser = require('xml2json');
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Organization = mongoose.model('organization');
@@ -12,9 +13,9 @@ let config = require('./../config');
 let fetchCreditReport = ({ productCode, searchField, searchValue }) => {
   return new Promise(async (resolve, reject) => {
     try {
-      // productCode = 'UEBV';
-      // searchField = 'ABN';
-      // searchValue = '38881083819';
+      productCode = 'HXBCA';
+      searchField = 'ABN';
+      searchValue = '51069691676';
       const organization = await Organization.findOne({
         isDeleted: false,
       })
@@ -65,11 +66,16 @@ let fetchCreditReport = ({ productCode, searchField, searchValue }) => {
       Logger.log.info('Making a request to illion at', new Date());
       // console.log(url, xmlBody, options);
       let { data } = await axios.post(url, xmlBody, options);
-      const jsonData = convert.xml2js(data);
+      Logger.log.info('Successfully received report at', new Date());
+      const jsonData = parser.toJson(data);
       Logger.log.info('Successfully received report at', new Date());
 
-      // console.log('converted JSON::', JSON.stringify(jsonData, null, 3));
-      // fs.writeFileSync('uebv.json', JSON.stringify(jsonData));
+      // console.log('converted JSON::', jsonData);
+      console.log(
+        'converted JSON::',
+        JSON.stringify(JSON.parse(jsonData), null, 3),
+      );
+      fs.writeFileSync('hxbca1.json', jsonData);
       return resolve(jsonData);
     } catch (e) {
       console.log('Error in getting entity details from ABN');
@@ -78,6 +84,71 @@ let fetchCreditReport = ({ productCode, searchField, searchValue }) => {
     }
   });
 };
+// fetchCreditReport({});
+// let report = null;
+
+let processObj = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'object' && obj[key].length === undefined) {
+      console.log('Calling for::', key);
+      obj[key] = processObj(obj[key]);
+      if (obj[key].hasOwnProperty('Year')) {
+        obj[key + 'Str'] = processDate(obj[key]);
+      }
+      console.log('Processed for::', key);
+    } else if (typeof obj[key] === 'object' && obj[key].length > 0) {
+      console.log('Calling for::', key);
+      obj[key].forEach((subObj) => {
+        if (typeof subObj === 'object' && subObj.length === undefined) {
+          subObj = processObj(subObj);
+        }
+      });
+    }
+    console.log('returning object::');
+  });
+  return obj;
+};
+
+let processDate = (obj) => {
+  console.log('OBJ in DATE::', obj);
+  if (
+    !obj.hasOwnProperty('Year') ||
+    !obj.hasOwnProperty('Month') ||
+    !obj.hasOwnProperty('Day')
+  )
+    return '';
+  let dt = new Date(obj['Year'], parseInt(obj['Month']) - 1, obj['Day']);
+  if (
+    obj.hasOwnProperty('Hour') &&
+    obj.hasOwnProperty('Minute') &&
+    obj.hasOwnProperty('Second')
+  )
+    dt.setHours(obj['Hour'], obj['Minute'], obj['Second']);
+  console.log('Converted Date::', dt);
+  return dt;
+};
+
+let processIllionReport = (report) => {
+  report = fs.readFileSync('hxbca1.json', 'utf8');
+  report = JSON.parse(report);
+  console.log(typeof report);
+  console.log(JSON.stringify(report, null, 3));
+  Object.keys(report).forEach((key) => {
+    if (typeof report[key] === 'object' && report[key].length === undefined) {
+      console.log('Calling for::', key);
+      report[key] = processObj(report[key]);
+      console.log('Processed for::', key, report[key]);
+      if (report[key].hasOwnProperty('Year')) {
+        report[key + 'Str'] = processDate(report[key]);
+      }
+    }
+    // console.log('report[key] after::', report[key]);
+  });
+  console.log('Illion report processed::', JSON.stringify(report, null, 3));
+  // report['soap:Envelope']
+};
+
+processIllionReport();
 
 module.exports = {
   fetchCreditReport,
