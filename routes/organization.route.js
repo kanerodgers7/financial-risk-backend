c3330e2da53affdab12fdc03dfc52e7b2cb68cda
kanerodgers7 <jellyfish9629@gmail.com>
2021-11-10 14:38:35 +0530

    Added Company Details in Client Org route

--- routes/organization.route.js
+++ routes/organization.route.js
@@ -5,6 +5,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Organization = mongoose.model('organization');
+const Client = mongoose.model('client');
 
 /*
  * Local Imports
@@ -16,11 +17,25 @@ const Logger = require('./../services/logger');
  */
 router.get('/details', async function (req, res) {
   try {
-    const organization = await Organization.findOne({
+    const promiseArr = [];
+    promiseArr.push(Organization.findOne({
       isDeleted: false,
     })
       .select({ name: 1, website: 1, contactNumber: 1, address: 1, email: 1 })
-      .lean();
+      .lean());
+    promiseArr.push(Client.findOne({
+      _id: req.user.clientId,
+    })
+      .select({ riskAnalystId: 1, serviceManagerId: 1 })
+      .populate([{ path: 'riskAnalystId', options: {select: 'name'}}, { path: 'serviceManagerId', options: {select: 'name'}}])
+      .lean());
+    const [organization, client] = await Promise.all(promiseArr);
+    if(client && client.riskAnalystId && client.riskAnalystId.name) {
+      organization.riskAnalyst = client.riskAnalystId.name;
+    }
+    if(client && client.serviceManagerId && client.serviceManagerId.name) {
+      organization.serviceManager = client.serviceManagerId.name;
+    }
     res.status(200).send({ status: 'SUCCESS', data: organization });
   } catch (e) {
     Logger.log.error(
