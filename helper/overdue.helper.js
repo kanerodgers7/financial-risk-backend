81dbc30cf422655ce157ea6f23cd62eb9b482270
kanerodgers7 <jellyfish9629@gmail.com>
2022-05-24 16:20:39 +0530

    fixed helper to check date range

--- helper/overdue.helper.js
+++ helper/overdue.helper.js
@@ -913,14 +913,12 @@ const checkDateRange = async ({
     const headers = [];
     const startYear = new Date(startDate).getFullYear();
     const endYear = new Date(endDate).getFullYear();
-    const startingMonth = new Date(startDate).getMonth();
-    const endingMonth = new Date(endDate).getMonth();
+    const startingMonth = new Date(startDate).getMonth() + 1;
+    const endingMonth = new Date(endDate).getMonth() + 1;
     const filters = [
       {
         label: 'Date',
-        value: `${monthString[startingMonth + 1]} ${startYear} to ${
-          monthString[endingMonth + 1]
-        } ${endYear}`,
+        value: `${monthString[startingMonth]} ${startYear} to ${monthString[endingMonth]} ${endYear}`,
         type: 'string',
       },
     ];
