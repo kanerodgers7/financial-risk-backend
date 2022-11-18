4e5aefababbd7f5391408694c0a426b3477fb9f5
kanerodgers7 <jellyfish9629@gmail.com>
2022-11-18 18:22:53 +0530

    filter by debtor name in credit limit section and in overdues updated

--- routes/entitySearch.route.js
+++ routes/entitySearch.route.js
@@ -34,6 +34,8 @@ router.get('/', async function (req, res) {
     let response = [];
     if (req.query.entityType === 'debtorIds') {
       req.query.entityType = 'debtors';
+    } else if (req.query.entityType === 'debtorId') {
+      req.query.entityType = 'debtors';
     }
     if (req.query.searchString) {
       switch (req.query.entityType) {
